26c3a41d639c2ab3f708960b29e2d050f6d1ec74
awo-dev <anders@wiberg.tech>
2018-03-06 10:08:04 +0100

    Add authentication test endpoint

--- src/main/java/Error.java
+++ src/main/java/Error.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Error {
+    private String error_type;
+    private String error_message;
+
+    public Error() { }
+
+    public Error(String error_message) {
+        this.error_message = error_message;
+    }
+
+    public Error(String error_type, String error_message) {
+        this.error_type = error_type;
+        this.error_message = error_message;
+    }
+
+    public String getError_type() {
+        return error_type;
+    }
+
+    public void setError_type(String error_type) {
+        this.error_type = error_type;
+    }
+
+    public String getError_message() {
+        return error_message;
+    }
+
+    public void setError_message(String error_message) {
+        this.error_message = error_message;
+    }
 }
--- src/main/java/hellorest.java
+++ src/main/java/hellorest.java
@@ -1,13 +1,49 @@
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import authwrapper.dto.Speed;
+import authwrapper.dto.User;
+import authwrapper.helper.UserAdministrationFactory;
+import authwrapper.transport.AuthenticationException;
+import authwrapper.transport.ConnectivityException;
+import authwrapper.transport.IUserAdministration;
+import com.google.gson.Gson;
 
 @Path("hello")
 public class hellorest {
+    private final Gson gson = new Gson();
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String test() {
-        return "Hello, World!";
+    public Response test() {
+        return Response.ok("Hello, World!").build();
+    }
+
+    @POST
+    @Path("authenticate")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response authenticate(@FormParam("username") String username, @FormParam("password") String password) {
+        final IUserAdministration userAdmin;
+        final User user;
+
+        try {
+            userAdmin = UserAdministrationFactory.getUserAdministration(Speed.LUDICROUS_SPEED);
+            user = userAdmin.authenticateUser(username, password);
+        } catch (ConnectivityException e) {
+            Error err = new Error();
+            err.setError_type("Connectivity Error");
+            err.setError_message(e.getMessage());
+
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(gson.toJson(err)).build();
+        } catch (AuthenticationException e) {
+            Error err = new Error();
+            err.setError_type("Authentication Error");
+            err.setError_message(e.getMessage());
+
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(gson.toJson(err)).build();
+        }
+
+        return Response.ok().entity(gson.toJson(user)).build();
     }
 }
