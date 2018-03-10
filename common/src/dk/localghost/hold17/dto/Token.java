25988b0a7f1a0bdc9368bc2dd455d3680b89b7e2
awo-dev <anders@wiberg.tech>
2018-03-10 19:08:14 +0100

    Create tokens

---
+++ common/src/dk/localghost/hold17/dto/Token.java
@@ -0,0 +1,44 @@
+package dk.localghost.hold17.dto;
+
+import dk.localghost.authwrapper.dto.User;
+
+public class Token {
+    private String access_token;
+    private String token_type;
+    private long expires_in;
+    private User user;
+
+    public Token() { }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    public void setToken_type(String token_type) {
+        this.token_type = token_type;
+    }
+
+    public long getExpires_in() {
+        return expires_in;
+    }
+
+    public void setExpires_in(long expires_in) {
+        this.expires_in = expires_in;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
