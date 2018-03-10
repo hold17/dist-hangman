222e4cab6e0a6ca3b9ace3dd325d3c08fa4c57d4
awo-dev <anders@wiberg.tech>
2018-03-10 17:18:34 +0100

    Add a class to manage all routes

---
+++ rest-server/src/dk/localghost/hold17/rest/config/Routes.java
@@ -0,0 +1,8 @@
+package dk.localghost.hold17.rest.config;
+
+public class Routes {
+    // ==== Authentication ====
+    public final static String AUTH_ROOT = "auth";
+    public final static String AUTH_AUTHENTICATION = "authentication";
+    public final static String AUTH_VALIDATE = "validate";
+}
