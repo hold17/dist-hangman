5fadfa9d5d834e06c83f209a178f04cf200f9060
awo-dev <anders@wiberg.tech>
2018-03-12 00:53:19 +0100

    Cleanup hangman

---
+++ common/src/dk/localghost/hold17/helpers/HangmanHelper.java
@@ -0,0 +1,34 @@
+package dk.localghost.hold17.helpers;
+
+import dk.localghost.authwrapper.dto.User;
+import dk.localghost.hold17.dto.HangmanGame;
+import dk.localghost.hold17.dto.Token;
+import dk.localghost.hold17.transport.IAuthentication;
+import dk.localghost.hold17.transport.IHangman;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HangmanHelper {
+    public static IHangman getHangmanService(Token token) {
+        IAuthentication auth = null;
+        URL hangUrl = null;
+        try {
+            auth = AuthorizationHelper.getAuthService();
+            token.setUser(auth.getUserFromToken(token.getAccess_token()));
+
+            hangUrl = new URL( auth.getHangmanService(token.getUser()) + "?wsdl");
+        } catch(MalformedURLException ex) {
+            // TODO: Remove after Sebastian's commit
+        }
+
+        QName hangQname = new QName("http://server.hold17.localghost.dk/", "HangmanLogicService");
+        Service hangSer = Service.create(hangUrl, hangQname);
+
+        IHangman hangman = hangSer.getPort(IHangman.class);
+
+        return hangman;
+    }
+}
--- rest-server/src/dk/localghost/hold17/rest/api/hangman/Hangman.java
+++ rest-server/src/dk/localghost/hold17/rest/api/hangman/Hangman.java
@@ -1,65 +1,42 @@
 package dk.localghost.hold17.rest.api.hangman;
 
 import com.google.gson.Gson;
-import dk.localghost.authwrapper.dto.User;
 import dk.localghost.hold17.dto.HangmanGame;
-import dk.localghost.hold17.helpers.AuthorizationHelper;
+import dk.localghost.hold17.dto.Token;
+import dk.localghost.hold17.helpers.HangmanHelper;
 import dk.localghost.hold17.rest.auth.AuthenticationEndpoint;
-import dk.localghost.hold17.transport.IAuthentication;
 import dk.localghost.hold17.transport.IHangman;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 @Path("hangman")
 public class Hangman {
-
     private static final Gson gson = new Gson();
-    private HangmanGame game = new HangmanGame();
-    private IAuthentication auth;
-    private IHangman hangman;
-    private User user;
 
-    private String address = "127.0.0.1";
-    private int port = 1337;
-
-//    @AuthenticationEndpoint.Auth
-    @GET
+    @AuthenticationEndpoint.Auth
+    @POST
     @Path("game")
     @Produces(MediaType.APPLICATION_JSON)
-//    public Response startGame(@Context HttpServletRequest servletRequest) {
-    public Response startGame() {
-        final String authSERVICE = "auth";
-        user = new User("s165242", "mismismis");
-        try {
-//            final String header = servletRequest.getHeader(HttpHeaders.AUTHORIZATION);
-//            final String token = header.substring("Bearer ".length()).trim();
-//            user = AuthorizationHelper.getAuthService().getUserFromToken(token);
+    public Response startGame(@Context HttpServletRequest servletRequest) {
+        final String header = servletRequest.getHeader(HttpHeaders.AUTHORIZATION);
+        final String tokenStr = header.substring("Bearer ".length()).trim();
 
-            final URL authURL = new URL("http://" + address + ":" + port + "/" + authSERVICE + "?wsdl");
-            QName qname = new QName("http://server.hold17.localghost.dk/", "AuthenticationService");
-            Service ser = Service.create(authURL, qname);
-            auth = ser.getPort(IAuthentication.class);
+        Token token = new Token();
+        token.setAccess_token(tokenStr);
 
-            final URL hangUrl = new URL( auth.getHangmanService(user) + "?wsdl");
-            QName hangQname = new QName("http://server.hold17.localghost.dk/", "HangmanLogicService");
-            Service hangSer = Service.create(hangUrl, hangQname);
-            hangman = hangSer.getPort(IHangman.class);
-            game.setVisibleWord(hangman.getVisibleWord());
-        } catch(MalformedURLException ex) {
+        IHangman hangman = HangmanHelper.getHangmanService(token);
 
-        }
+        HangmanGame game = new HangmanGame();
+        game.setVisibleWord(hangman.getVisibleWord());
         System.out.println(hangman.getWord());
-        return Response.ok(gson.toJson(game)).build();
+
+        return Response.status(Response.Status.CREATED).entity(gson.toJson(game)).build();
     }
 }
