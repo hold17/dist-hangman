f23f918f57cccac0ff88080111adc08e1e1602b8
awo-dev <anders@wiberg.tech>
2018-03-12 23:32:43 +0100

    Add CORS to REST Server

---
+++ rest-server/src/dk/localghost/hold17/rest/config/CORSFilter.java
@@ -0,0 +1,15 @@
+package dk.localghost.hold17.rest.config;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+public class CORSFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+    }
+}
